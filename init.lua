6810944938bd3415c1a205a26d418a04731c12f0
Tahmid A. Khan <60913202+tahmid-khan@users.noreply.github.com>
2025-04-12 21:24:07 +0600

    update keymap

--- init.lua
+++ init.lua
@@ -189,16 +189,21 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<C-S-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-S-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-S-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-S-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
--- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
--- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
--- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
--- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+-- getting into normal mode
+vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('c', 'jj', '<Esc>')
+
+-- getting into command mode
+vim.keymap.set('n', 'k;', ':')
+vim.keymap.set('n', '<D-;>', ':')
+vim.keymap.set('n', '<D-k>', ':')
+vim.keymap.set('n', '<C-;>', ':')
+vim.keymap.set('n', '<C-k>', ':')
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
